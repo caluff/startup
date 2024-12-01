86baa59f9447a31f921581c2d3c412707ef42fe9
Daniel Caluff <dcaluff@gmail.com>
2024-11-30 22:15:21 -0500

    add sanity

---
+++ types.ts
@@ -0,0 +1,15 @@
+type Author = {
+  _id: number;
+  name: string;
+};
+
+export type StartupCardType = {
+  _createdAt: Date;
+  views: number;
+  author: Author;
+  _id: number;
+  description: string;
+  image: string;
+  category: string;
+  title?: string;
+};
