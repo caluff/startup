78efefe02ddf2830b8c5b7feba7164731d541e12
Daniel Caluff <dcaluff@gmail.com>
2025-01-12 16:43:21 -0500

    update UI

--- components/view.tsx
+++ components/view.tsx
@@ -12,7 +12,7 @@ export const View = async ({id}: { id: string }) => {
 
   return (
     <div className={'view-container'}>
-      <div className={'absolute - top-2 -right-2'}>
+      <div className={'absolute -top-2 -right-2'}>
         <Ping/>
       </div>
       <p className={'view-text'}>
